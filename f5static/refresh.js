0cf25680f14bf0e875316a95d5b36120fa3f07cf
UncleBill <billbill290@gmail.com>
2013-04-29 11:03:59 +0800

    fix: reset fresh script to stable version

--- f5static/refresh.js
+++ f5static/refresh.js
@@ -3,7 +3,8 @@ var pathname = location.pathname;   // a prefix
 
 var getFileAttachers = function(){
     var tags,
-        tagSize;
+        tagSize,
+        attachers = [];
 
     if( document.querySelectorAll ){
         tags = document.querySelectorAll("*[href],*[src]");
@@ -13,23 +14,21 @@ var getFileAttachers = function(){
 
     var localHref = location.href;
         tagSize = tags.length;
-    var attachers = new Array( tagSize );
     for( var i = 0;i < tagSize; ++i ){
         var tag = tags[i];
         if( tag.src ){
-            attachers[i] = {
+            attachers.push({
                 element:tag,
                 file:  decodeURIComponent(tag.src)
-            }
+            })
         } else if ( tag.href && tag.href !== localHref + "#" ){
-            attachers[i] = {
+            attachers.push({
                 element:tag,
                 file:  decodeURIComponent(tag.href)
-            }
+            });
         }
     }
 
-    console.log( attachers );
     return attachers;
 }
 
@@ -49,7 +48,7 @@ var reloadTag = function( attcher ){
 attachers = getFileAttachers();
 socket.on('reload', function ($data) {
     pathname = decodeURIComponent( pathname );
-    console.log( "log:$data",$data );
+    //console.log( "log:$data",$data );
     if( pathname === $data.slice(1) ){       // type of $data is ./foo/bar/file.html
         window.location.reload();
     } else {
@@ -57,7 +56,7 @@ socket.on('reload', function ($data) {
             var url = location.protocol + "//" + location.host + $data.slice(1);
             if(url == attachers[i].file) {
                 reloadTag( attachers[i] );
-                console.log( "log:file", attachers.file );
+                //console.log( "log:file", attachers.file );
             }
         }
     }
